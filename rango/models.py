bbfc8cfe02b63445fb652c7802ecb207fc5657e5
Justin-CC <justinchansw@gmail.com>
2023-03-12 01:42:03 +0000

    update models

--- rango/models.py
+++ rango/models.py
@@ -3,32 +3,47 @@ from django.template.defaultfilters import slugify
 
 # # Create your models here.
 # 
-# class Category(models.Model):
-#     NAME_MAX_LENGTH = 128
-# 
-#     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
-#     views = models.IntegerField(default=0)
-#     likes = models.IntegerField(default=0)
-#     slug = models.SlugField(unique=True)
-# 
-#     def save(self, *args, **kwargs):
-#         self.slug = slugify(self.name)
-#         super(Category, self).save(*args, **kwargs)
-# 
-#     class Meta:
-#         verbose_name_plural = 'Categories'
-# 
-#     def __str__(self):
-#         return self.name
-# 
-# class Page(models.Model):
-#     TITLE_MAX_LENGTH = 128
-#     URL_MAX_LENGTH = 200
-# 
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=TITLE_MAX_LENGTH)
-#     url = models.URLField()
-#     views = models.IntegerField(default=0)
-# 
-#     def __str__(self):
-#         return self.title
+class Dish(models.Model):
+    NAME_MAX_LENGTH = 128
+
+    dishid = models.IntegerField(primary_key = True )
+    dishname = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=30)
+    price = models.IntegerField()
+    description = models.CharField(max_length=500)
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super(Category, self).save(*args, **kwargs)
+    #
+    # class Meta:
+    #     verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.dishname
+
+class User(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
+    userid = models.IntegerField(primary_key = True )
+    username = models.CharField(max_length=10, unique=True)
+    phone = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
+class Comment(models.Model):
+
+
+    commentid = models.IntegerField(primary_key = True )
+    dish = models.ForeignKey("Dish", on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    date = models.DateField(auto_now_add=False)
+
+    def __str__(self):
+        return self.commentid
+
+
