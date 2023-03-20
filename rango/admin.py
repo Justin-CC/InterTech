d77e740d2207631d02a210dace09c6d9ac6834dd
Justin-CC <justinchansw@gmail.com>
2023-03-20 00:57:39 +0000

    update user imput and models

--- rango/admin.py
+++ rango/admin.py
@@ -5,13 +5,21 @@ from rango.models import Dish, User, Comment
 #
 # class PageAdmin(admin.ModelAdmin):
 #     list_display = ('title', 'category', 'url')
-# 
+#
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('userid','username', 'phone', 'email')
+
 class DishAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('dishname',)}
+    list_display = ('dishid', 'dishname', 'type', 'price')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commentid','dish', 'user')
 
 
 admin.site.register(Dish, DishAdmin)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Comment)
 
 # admin.site.register(Page, PageAdmin)
